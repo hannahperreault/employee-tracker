7e14d9476d6bec9f4ee14f351f5df86217ae789d
Hannah Perreault <hperre@gmail.com>
2020-06-20 23:15:05 -0400

    added inquirer function start(); with console.logs instead of functions for actions chosen

--- server.js
+++ server.js
@@ -14,5 +14,36 @@ connection.connect((err, res) => {
   if (err) {
     throw err;
   }
-  console.log("connected");
+  start();
 });
+
+// function which prompts the user for what action they should take
+const start = () => {
+  inquirer
+    .prompt({
+      name: "chooseAction",
+      type: "list",
+      message:
+        "Would you like to [VIEW] all records, [ADD] a record, or [UPDATE] an existing record?",
+      choices: ["VIEW", "ADD", "UPDATE", "EXIT"],
+    })
+    .then(function (answer) {
+      // based on their answer, either call the bid or the post functions
+      if (answer.chooseAction === "VIEW") {
+        //function to view records
+        //   viewRecords();
+        console.log("viewing record");
+      } else if (answer.chooseAction === "ADD") {
+        //function to add record
+        //   addRecord();
+        console.log("adding record");
+      } else if (answer.chooseAction === "UPDATE") {
+        //function to update record
+        //updateRecord();
+
+        console.log("updating record");
+      } else {
+        connection.end();
+      }
+    });
+};
